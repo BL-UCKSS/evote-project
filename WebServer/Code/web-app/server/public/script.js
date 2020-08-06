94fa3600970c7a128a62129a998f4b8e85a63629
johndonggyu <johndonggyu@naver.com>
2020-08-07 06:19:00 +0900

    관리자 선거 등록 시 날짜, 시간 모두 선택 가능하도록 구현함

--- WebServer/Code/web-app/server/public/script.js
+++ WebServer/Code/web-app/server/public/script.js
@@ -29,3 +29,11 @@ var hideToggle = function(hide, toggle){
 var goUrl = function(url){
     document.location.href=url;
 };
+function getFormatDate(date, num){
+    var year = date.getFullYear();              //yyyy
+    var month = (1 + date.getMonth());          //M
+    month = month >= 10 ? month : '0' + month;  //month 두자리로 저장
+    var day = date.getDate() + num;                   //d
+    day = day >= 10 ? day : '0' + day;          //day 두자리로 저장
+    return  year + '-' + month + '-' + day;       //'-' 추가하여 yyyy-mm-dd 형태 생성 가능
+}
