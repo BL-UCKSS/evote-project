58d7e737410010fdddb7309066dde83b3a4b5fdb
johndonggyu <johndonggyu@naver.com>
2020-08-08 08:29:37 +0900

    체인코드 querystring error 수정

--- chaincode/테스트코드/contract/lib/voterContract.js
+++ chaincode/테스트코드/contract/lib/voterContract.js
@@ -312,10 +312,9 @@ class MyAssetContract extends Contract {
   async queryWithQueryString(ctx, queryString) {
 
     console.log('query String');
-    console.log(JSON.stringify(queryString));
+    //console.log(JSON.stringify(queryString));
 
-    let resultsIterator = await ctx.stub.getQueryResult(queryString);
-
+    let resultsIterator = await ctx.stub.getQueryResult(JSON.stringify(queryString));
     let allResults = [];
 
     // eslint-disable-next-line no-constant-condition
