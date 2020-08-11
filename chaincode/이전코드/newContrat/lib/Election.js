d41b13b6a7e1d27c20fc1f0761ad9bc9a2c9872e
davidBBU <forevermomo28@gmail.com>
2020-08-12 00:44:36 +0900

    ******관리자기능 + vBallot  생성 + castvote 구현 완료
    (chaincode폴더정리 -> 현재 진행중인 폴더=1.1에서 1.0.5로 넘어왔습니다
    앞으로 넘버링을 폴더랑 컨트랙트 파일이랑 같이가고, 이전 파일들은 꾸준히 <이전코드>에 넣어주시면 됩니다.)

--- chaincode/이전코드/newContrat/lib/Election.js
+++ chaincode/이전코드/newContrat/lib/Election.js
@@ -3,11 +3,7 @@
 class Election {
 
   /**
-   *
-   * validateElection
-   *
-   * check for valid election, cross check with government. Don't want duplicate
-   * elections.
+
    *
    * @param electionId - an array of choices
    * @returns - yes if valid Voter, no if invalid
@@ -23,18 +19,7 @@ class Election {
       return false;
     }
   }
-  /**
-   *
-   * Election
-   *
-   * Constructor for an Election object. Specifies start and end date.
-   *
-   * @param items - an array of choices
-   * @param election - what election you are making ballots for
-   * @param voterId - the unique Id which corresponds to a registered voter
-   * @returns - registrar object
-   */
-  constructor(name, country, year, startDate, endDate) {
+  constructor(name, univ, startDate, endDate) {
     // 28자리 임의의 스트링 출력
     this.electionId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
@@ -42,19 +27,15 @@ class Election {
 
       //create the election object 객체 안에 들어가는 값들
       this.name = name;
-      this.country = country;
-      this.year = year;
+      this.univ = univ;
       this.startDate = startDate;
       this.endDate = endDate;
       this.type = 'election';
-      if (this.__isContract) {
-        delete this.__isContract;
-      }
       return this;
 
     } else {
       
-      throw new Error('not a valid election!');
+      throw new Error('선거 아이디가 생성되지 못했습니다!');
     }
 
   }
