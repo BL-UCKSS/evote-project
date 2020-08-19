5858b663eac202e2bdc1d8a9fc49e02c026469d4
johndonggyu <johndonggyu@naver.com>
2020-08-20 04:40:49 +0900

    불필요 함수 제거 및 필요 css 추가(로딩UI)

--- WebServer/Code/web-app/server/public/script.js
+++ WebServer/Code/web-app/server/public/script.js
@@ -4,16 +4,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable indent */
 /* eslint-disable strict */
-//adminMain
-var noElection = function (option) {
-    if (option === 'manage') { //선거 관리하기
-        document.location.href = '/adminManage';
-    } else if (option === 'count') { //투표율 확인
-        document.location.href = '/adminNow';
-    } else { //투표가 하나라도 개설되지 않았을 경우 팝업
-        $('#noElectionModal').modal('toggle');
-    }
-}
 //adminManage
 var changeTitle = function(modal, title, electionid){
     $('#'+modal).modal('toggle');
